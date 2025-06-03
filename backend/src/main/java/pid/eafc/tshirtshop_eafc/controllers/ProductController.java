a9bcaf8c245f861be1e3ffc600bf9a27ee5187fb
Add9Dev <adnane.aza@gmail.com>
2025-06-03 14:36:42 +0200

    fix

--- backend/src/main/java/pid/eafc/tshirtshop_eafc/controllers/ProductController.java
+++ backend/src/main/java/pid/eafc/tshirtshop_eafc/controllers/ProductController.java
@@ -1,14 +1,13 @@
 package pid.eafc.tshirtshop_eafc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 import pid.eafc.tshirtshop_eafc.model.Product;
 import pid.eafc.tshirtshop_eafc.repository.ProductRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -25,5 +24,12 @@ public class ProductController {
     @GetMapping("/byCategory")
     public List<Product> getProductsByCategory(@RequestParam Long categoryId) {
         return productRepository.findByCategoryId(categoryId);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
